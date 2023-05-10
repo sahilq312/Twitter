40914f0572d45927ec8116ede374b0ba91c140fd
Sahil <quraishisahil04@outlook.com>
2023-05-10 20:16:40 +0530

    Update Auth.js

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -4,10 +4,9 @@ import User from "../models/User.js";
 import bcrypt from "bcrypt";
 const saltRounds = 10;
 
-
-authController.post('/register', async(req,res)=> {
-    const newUser = new User(req.body);
-    const {username} = req.body;
+authController.post("/register", async (req, res) => {
+  const newUser = new User(req.body);
+  const { username } = req.body;
   try {
     // addition new
     const oldUser = await User.findOne({ username });
@@ -15,27 +14,27 @@ authController.post('/register', async(req,res)=> {
     if (oldUser)
       return res.status(400).json({ message: "User already exists" });
     // changed
-    else{
+    else {
       const user = await newUser.save();
-    res.status(200).json({ message:"registreation successfull" });
+      res.status(200).json({ message: "registreation successfull" });
     }
-    
-    
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-authController.post("/login", async(req,res)=>{
-    const { email,password} = req.body;
-    console.log(email,password);
-    const isExisting = await User.findOne({email,password})
-    if(isExisting){
-      res.status(200).json({messsage:'logged in'});
+authController.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  console.log(email, password);
+  try {
+    const isExisting = await User.findOne({ email });
+    if (isExisting) {
+      
+      res.status(200).json({ messsage: "logged in" });
     }
-    else{
-      res.status(500).json({message: "no user found"})
-    }
-})
+  } catch (error) {
+    res.status(500).json({ message: "no user found" });
+  }
+});
 
 export default authController;
