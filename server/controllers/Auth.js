f7ff43e0800d25c1d534096bc5a7ad140743e81e
Sahil <quraishisahil04@outlook.com>
2023-05-16 17:55:42 +0530

    update with bcrypt

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -5,36 +5,45 @@ import bcrypt from "bcrypt";
 const saltRounds = 10;
 
 authController.post("/register", async (req, res) => {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPass = await bcrypt.hash(req.body.password, salt);
+  req.body.password = hashedPass
   const newUser = new User(req.body);
-  const { username } = req.body;
+  const {username} = req.body
   try {
     // addition new
     const oldUser = await User.findOne({ username });
 
     if (oldUser)
       return res.status(400).json({ message: "User already exists" });
+
     // changed
-    else {
-      const user = await newUser.save();
-      res.status(200).json({ message: "registreation successfull" });
-    }
+    const user = await newUser.save();
+    res.status(200).json({ user });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
-
 authController.post("/login", async (req, res) => {
   const { email, password } = req.body;
-  console.log(email, password);
+
   try {
-    const isExisting = await User.findOne({ email });
-    if (isExisting) {
-      
-      res.status(200).json({ messsage: "logged in" });
+    const user = await User.findOne({ email });
+
+    if (user) {
+      const validity = await bcrypt.compare(password, user.password);
+
+      if (!validity) {
+        res.status(400).json("wrong password");
+      } else {
+        res.status(200).json("logged in");
+      }
+    } else {
+      res.status(404).json("User not found");
     }
-  } catch (error) {
-    res.status(500).json({ message: "no user found" });
+  } catch (err) {
+    res.status(500).json({message:"wrong"});
   }
-});
+})
 
 export default authController;
