000b28c468ed99bdecabd05d271836fba1121267
Sahil <quraishisahil04@outlook.com>
2023-05-18 20:53:48 +0530

    sign in page

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='bg-black  text-white flex items-center min-h-screen '>
+      <div className='relative flex flex-col m-auto space-y-8 bg-transparent shadow-2xl rounded-2xl md:flex-row md:space-y-0'>
+      <div className="flex flex-col justify-center p-8 md:p-14">
+          <h1 class="bg-gradient-to-r from-pink-600 to-fuchsia-900 bg-clip-text text-transparent mb-6 h-12 text-4xl font-semibold">Sign in</h1>
+          <form>
+          <span className="font-light text-gray-400 mb-8">
+            Welcome back! Please enter your details
+          </span>
+          <div class="py-4">
+            <span class="mb-2 text-md">Email</span>
+            <input
+              type="text"
+              className="w-full p-2 bg-[#121417] rounded-md placeholder:font-light placeholder:text-gray-500"
+              name="email"
+              id="email"
+              placeholder='Enter your email'
+            />
+          </div>
+          <div class="py-4">
+            <span class="mb-2 text-md">Password</span>
+            <input
+              type="password"
+              name="pass"
+              id="pass"
+              placeholder='Enter your password'
+              class="w-full p-2 bg-[#121417] rounded-md placeholder:font-light placeholder:text-gray-500"
+            />
+          </div>
+          <div class="flex justify-between w-full py-4"> 
+          </div>
+          <button
+            class="w-full bg-gradient-to-r from-pink-600 to-fuchsia-900 font-semibold text-white p-2 rounded-lg mb-6"
+          >
+            Sign in
+          </button>
+          </form>
+          <div class="text-center text-gray-400">
+            Dont'have an account?
+            <a href='/auth/register' class="font-bold text-white">Register for free</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
