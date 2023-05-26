5e065068c9178a0b844aecfe7edf1162b4b5355a
Sahil <quraishisahil04@outlook.com>
2023-05-27 00:06:55 +0530

    Update user API

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -1,35 +1,39 @@
 import express from "express";
-const userController = express.Router();
 import User from "../models/User.js";
 
-userController.getUser("/:id", (req, res) => {
+const userController = express.Router();
+
+// Get user
+userController.get("/getuser/:id", async (req, res) => {
   const userID = req.params.id;
 
   try {
-    const user = User.findById(id);
-    if (user) {
-      const { password, ...otherdetail } = user._doc;
+    const user = await User.findById(userID);
 
-      res.status(200).json(otherdetail);
+    if (user) {
+      const { password, ...otherDetail } = user._doc;
+
+      res.status(200).json(otherDetail);
     } else {
-      res.status(404).son("no such user");
+      res.status(404).json("No such user");
     }
   } catch (error) {
-    res;
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
-//delete user
-userController.delete("/:id", (req, res) => {
+// Delete user
+userController.delete("/delete/:id", (req, res) => {
   const userID = req.params.id;
-  User.findByIdAndDelete(userID).then((err) => {
-    //console.log("result")
-    if (err) {
-      res.json(err);
-    } else {
-      res.status(200).json({ message: "deleted" });
-    }
-  });
+
+  User.findByIdAndDelete(userID)
+    .then(() => {
+      res.status(200).json({ message: "Deleted" });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Internal server error" });
+    });
 });
 
 export default userController;
+
